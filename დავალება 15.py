e78fdcd6a69d304d5bfc5ffc2ff2cb6f234e82ca
vano tatulashvili <vanotimmy@gmail.com>
2023-02-18 13:42:40 -0800

    matplotlib chess code

--- დავალება 15.py
+++ დავალება 15.py
@@ -1,9 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 board = np.zeros((8, 8), dtype=int)
 
 board[1::2, ::2] = 1
 board[::2, 1::2] = 1
 
-for row in board:
-    print(*row)
+board[::2, 1::2] = 1
+board[1::2, ::2] = 1
+
+fig, ax = plt.subplots()
+ax.imshow(board, cmap='binary')
+
+ax.set_xlim([-0.5, 7.5])
+ax.set_ylim([-0.5, 7.5])
+
+ax.set_axis_off()
+
+plt.show()
